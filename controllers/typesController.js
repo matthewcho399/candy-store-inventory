bb55fb91db96ea6f044ae310f9d23b177dff709d
Matthew <matthewcho399@gmail.com>
2024-10-24 02:33:39 -0700

    db setup & create type form

--- controllers/typesController.js
+++ controllers/typesController.js
@@ -1,23 +1,24 @@
-const types = [
-  {
-    id: 0,
-    name: "chocolate",
-  },
-  {
-    id: 1,
-    name: "gummy",
-  },
-  {
-    id: 2,
-    name: "sour",
-  },
-  {
-    id: 3,
-    name: "caramel",
-  },
-];
+const db = require("../db/queries");
 
 const typesGet = (req, res) => {
+  const types = [
+    {
+      id: 0,
+      name: "chocolate",
+    },
+    {
+      id: 1,
+      name: "gummy",
+    },
+    {
+      id: 2,
+      name: "sour",
+    },
+    {
+      id: 3,
+      name: "caramel",
+    },
+  ];
   res.render("types/types", { types });
 };
 
@@ -29,8 +30,14 @@ const createTypeGet = (req, res) => {
   res.render("types/createType");
 };
 
+const createTypePost = (req, res) => {
+  console.log(req.body);
+  res.redirect("/types");
+};
+
 module.exports = {
   typesGet,
   singleTypeGet,
   createTypeGet,
+  createTypePost,
 };
--- routes/typesRouter.js
+++ routes/typesRouter.js
@@ -3,7 +3,10 @@ const controller = require("../controllers/typesController");
 const router = Router();
 
 router.get("/", controller.typesGet);
-router.get("/:candy", controller.singleTypeGet);
+
 router.get("/create", controller.createTypeGet);
+router.post("/create", controller.createTypePost);
+
+router.get("/:candy", controller.singleTypeGet);
 
 module.exports = router;
