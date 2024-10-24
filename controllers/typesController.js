ca367719385e3a0f01d65c5657c060cd4a5b3cca
Matthew <matthewcho399@gmail.com>
2024-10-24 02:47:45 -0700

    getTypes & creatType queries

--- controllers/typesController.js
+++ controllers/typesController.js
@@ -1,26 +1,9 @@
 const db = require("../db/queries");
 
-const typesGet = (req, res) => {
-  const types = [
-    {
-      id: 0,
-      name: "chocolate",
-    },
-    {
-      id: 1,
-      name: "gummy",
-    },
-    {
-      id: 2,
-      name: "sour",
-    },
-    {
-      id: 3,
-      name: "caramel",
-    },
-  ];
+async function typesGet(req, res) {
+  const types = await db.getTypes();
   res.render("types/types", { types });
-};
+}
 
 const singleTypeGet = (req, res) => {
   res.render("types/singleType");
@@ -30,10 +13,11 @@ const createTypeGet = (req, res) => {
   res.render("types/createType");
 };
 
-const createTypePost = (req, res) => {
-  console.log(req.body);
+async function createTypePost(req, res) {
+  const type = req.body.type;
+  await db.createType(type);
   res.redirect("/types");
-};
+}
 
 module.exports = {
   typesGet,
--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,15 @@
 const pool = require("./pool");
+
+async function getTypes() {
+  const { rows } = await pool.query("SELECT * FROM types");
+  return rows;
+}
+
+async function createType(type) {
+  await pool.query("INSERT INTO types (type) VALUES ($1)", [type]);
+}
+
+module.exports = {
+  getTypes,
+  createType,
+};
