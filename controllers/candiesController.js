a739db247e0263c761ec5fa94e84d83453c5cdc9
Matthew <matthewcho399@gmail.com>
2024-10-25 03:07:37 -0700

    candies get query logic

--- controllers/candiesController.js
+++ controllers/candiesController.js
@@ -1,33 +1,9 @@
-const candies = [
-  {
-    id: 0,
-    name: "Twix",
-    type: ["chocolate", "caramel"],
-    company: "Mars",
-    price: "2.50",
-    quantity: 15,
-  },
-  {
-    id: 1,
-    name: "Gummy Bear",
-    type: ["gummy"],
-    company: "Albanese",
-    price: "4.99",
-    quantity: 20,
-  },
-  {
-    id: 2,
-    name: "M&M",
-    type: ["chocholate"],
-    company: "M&M",
-    price: "2.99",
-    quantity: 50,
-  },
-];
+const db = require("../db/queries");
 
-const candiesGet = (req, res) => {
+async function candiesGet(req, res) {
+  const candies = await db.getCandies();
   res.render("candies/candies", { candies });
-};
+}
 
 const createCandyGet = (req, res) => {
   res.render("candies/createCandy");
--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,12 @@
 const pool = require("./pool");
 
+async function getCandies() {
+  const { rows } = await pool.query(
+    " SELECT candies.id AS id, candies.name AS name, candies.company, candies.quantity, candies.price, array_agg(types.type) AS types FROM candies JOIN candy_types ON candies.id = candy_types.candy_id JOIN types ON candy_types.type_id = types.id GROUP BY candies.id, candies.name, candies.company, candies.quantity, candies.price;"
+  );
+  return rows;
+}
+
 async function getTypes() {
   const { rows } = await pool.query("SELECT * FROM types");
   return rows;
@@ -17,6 +24,7 @@ async function createType(type) {
 }
 
 module.exports = {
+  getCandies,
   getTypes,
   getTypeByName,
   createType,
