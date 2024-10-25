652a4a623aa8c6b3c8464698ac52080821bdc2f8
Matthew <matthewcho399@gmail.com>
2024-10-24 17:55:20 -0700

    validation for types create

--- controllers/typesController.js
+++ controllers/typesController.js
@@ -1,4 +1,24 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters";
+const lengthErr = "must be between 1 and 20 characters";
+const duplicateErr = "Type already exists";
+
+const validateType = [
+  body("type")
+    .trim()
+    .isAlpha()
+    .withMessage(`Type ${alphaErr}`)
+    .isLength({ min: 1, max: 20 })
+    .withMessage(`Type ${lengthErr}`)
+    .custom(async (type) => {
+      const exists = await db.getTypeByName(type);
+      if (exists) {
+        return Promise.reject(duplicateErr);
+      }
+    }),
+];
 
 async function typesGet(req, res) {
   const types = await db.getTypes();
@@ -13,11 +33,20 @@ const createTypeGet = (req, res) => {
   res.render("types/createType");
 };
 
-async function createTypePost(req, res) {
-  const type = req.body.type;
-  await db.createType(type);
-  res.redirect("/types");
-}
+const createTypePost = [
+  validateType,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("types/createType", {
+        errors: errors.array(),
+      });
+    }
+    const type = req.body.type;
+    await db.createType(type);
+    res.redirect("/types");
+  },
+];
 
 module.exports = {
   typesGet,
--- db/queries.js
+++ db/queries.js
@@ -5,11 +5,19 @@ async function getTypes() {
   return rows;
 }
 
+async function getTypeByName(type) {
+  const { rows } = await pool.query("SELECT * FROM types WHERE type = $1", [
+    type,
+  ]);
+  return rows[0];
+}
+
 async function createType(type) {
   await pool.query("INSERT INTO types (type) VALUES ($1)", [type]);
 }
 
 module.exports = {
   getTypes,
+  getTypeByName,
   createType,
 };
