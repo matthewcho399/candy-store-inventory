d30ed18efd838e6924a71e468785acd264fe155b
Matthew <matthewcho399@gmail.com>
2024-11-14 00:08:41 -0800

    navbar styling

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("node:path");
 const homeRouter = require("./routes/homeRouter");
 const candiesRouter = require("./routes/candiesRouter");
 const typesRouter = require("./routes/typesRouter");
+const assetsPath = path.join(__dirname, "public");
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
